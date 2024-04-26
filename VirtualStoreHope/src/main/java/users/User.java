2c54f949dcdd74695c63e6891594d280140e338d
JavierBoyaca <144865347+JavierBoyaca@users.noreply.github.com>
2024-04-25 21:16:35 -0500

    Add files via upload

---
+++ VirtualStoreHope/src/main/java/users/User.java
@@ -0,0 +1,29 @@
+package users;
+
+public class User {
+
+    //atributos encapsulados con el midificador private
+
+
+    private int id;
+    private String name;
+    private String lastName;
+    private  String phone;
+    private  String mail;
+    private  String passWord;
+
+    //Constructor de la clase
+
+    public User(){
+
+    }
+
+    // getter and setter
+
+    public  void setId(int id){
+        this.id = id;
+    }
+
+
+
+}
